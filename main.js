d2ec8b5dd2d2e47c3dbfabbcf44da54623adfb45
RKcooldude10D5 <89266597+RKcooldude10D5@users.noreply.github.com>
2022-02-11 16:10:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup(){
+    canvas=createCanvas(600, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet');
+}
+function erasedrawing(){
+    background("white");
+}
+function draw(){
+    strokeWeight(2.5);
+    stroke("black");
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML='label: ' + results[0].label;
+    document.getElementById("accuracy").innerHTML='confidence: ' + Math.round(results[0].confidence * 100) + '%';
+    utterThis= new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
